e5dc7ef86a6ee63d87dc06cee0f609688e917eeb
Adetayo Olaitan <olaitan.adetayo@gmail.com>
2021-09-24 11:48:50 +0100

    added more auth methods

--- src/feature/auth/index.js
+++ src/feature/auth/index.js
@@ -1,12 +1,12 @@
 import { auth } from "../../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
 export const createUser = (email, password) => {
     createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    console.log("signed up => "+user)
+    console.log(`signed up => Name:${user.displayName} ,Email:${user.email}`)
     // ...
   })
   .catch((error) => {
@@ -16,4 +16,32 @@ export const createUser = (email, password) => {
     // ..
   });
 }
+
+export const signIn = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+.then((userCredential) => {
+  // Signed in 
+  const user = userCredential.user;
+  console.log(`signed up => Name:${user.displayName} ,Email:${user.email}`)
+  // ...
+})
+.catch((error) => {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  console.log("signed up error => "+errorMessage)
+  // ..
+});
+}
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
 
